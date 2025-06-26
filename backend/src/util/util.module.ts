8e698b15bcefcd667caacb1284999ea34aff02d7
Felipe Casa <felipe.alvesnn@gmail.com>
2025-06-26 12:17:01 -0300

    feat: adicionar configuração assíncrona do JwtModule no UtilModule

--- backend/src/util/util.module.ts
+++ backend/src/util/util.module.ts
@@ -1,8 +1,23 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { SessionGateway } from './session.gateway';
 
 @Global() // ✨ Torna disponível em toda aplicação
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn:
+            configService.get<string>('JWT_ACCESS_EXPIRES_IN') || '15m',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [SessionGateway],
   exports: [SessionGateway],
 })
