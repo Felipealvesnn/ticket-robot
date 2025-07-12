46d161407a70a314bed92e5dbc519eca46367ecd
Felipe Casa <felipe.alvesnn@gmail.com>
2025-07-12 10:23:12 -0300

    fix: corrigir formatação do tipo de retorno no método de serviço de tickets

--- backend/src/ticket/ticket.service.ts
+++ backend/src/ticket/ticket.service.ts
@@ -487,7 +487,7 @@ export class TicketService {
       messageType?: 'TEXT' | 'IMAGE' | 'AUDIO' | 'VIDEO' | 'DOCUMENT';
     },
   ): Promise<{
-     id: string;
+    id: string;
     // content: string;
     direction: 'OUTBOUND';
     // messageType: string;
